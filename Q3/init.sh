74fee78e18f2474c3ea487f10f962715477926ce
Ahmed Said <ahmed.el.sayeed1914@gmail.com>
2026-04-15 23:55:03 +0200

    update Q3

--- Q3/init.sh
+++ Q3/init.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-INIT_DIR="/tmp/init"
-
-echo "First initiating any namespaces"
-kubectl apply -f "$INIT_DIR/namespace.yaml"
-
-echo "Setting up the rest of the resources"
-kubectl apply -f "$INIT_DIR/*.yaml"
-
-rm -rf $INIT_DIR
+../utility/init.sh
