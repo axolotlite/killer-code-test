4ec1e643aa217a4582b7d04f2fc39d315bc87114
Ahmed Said <ahmsaid@deloitte.com>
2026-06-10 00:16:56 +0300

    use correct path for utility.sh

--- kubernetes-labs/serving-3-tier-app/step2/verify.sh
+++ kubernetes-labs/serving-3-tier-app/step2/verify.sh
@@ -3,7 +3,7 @@
 OUTPUT_FILE="${OUTPUT_FILE:-$HOME/validation.log}"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/../assets/utility.sh"
+source "$SCRIPT_DIR/utility.sh"
 
 NS="default"
 
